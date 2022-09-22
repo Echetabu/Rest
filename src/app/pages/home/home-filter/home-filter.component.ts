53bf8fbc53d136e2543cc2fa92b8589e74520d40
Kingsley Etoka <etokakingsley@gmail.com>
2022-09-22 13:10:09 +0100

    fixed filter section

--- src/app/pages/home/home-filter/home-filter.component.ts
+++ src/app/pages/home/home-filter/home-filter.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+const regions = [
+  "All",
+  "Asia",
+  "Africa"
+]
+
 @Component({
   selector: 'app-home-filter',
   templateUrl: './home-filter.component.html',
   styleUrls: ['./home-filter.component.scss']
 })
 export class HomeFilterComponent implements OnInit {
+  isOpen: Boolean = false;
+  selectedText: string = "Filter by Region";
+  
+  regions: string[] = regions;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleVisibility() {
+    this.isOpen = !this.isOpen;
+  }
+
+  setSelected(region: string){
+    this.isOpen = false;
+    this.selectedText = region;
   }
 
 }
