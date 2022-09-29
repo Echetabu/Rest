423a6b94a4d6d3220cb581589774f5b858814939
Kingsley Etoka <etokakingsley@gmail.com>
2022-09-29 19:28:10 +0100

    Added currency filter pipe

--- src/app/pages/detail/detail.module.ts
+++ src/app/pages/detail/detail.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { DetailComponent } from './detail.component';
 import { DetailRoutingModule } from './detail-routing.module';
 import { BackBtnComponent } from './back-btn/back-btn.component';
+import { DetailPipe } from './detail.pipe';
 
 
 
 @NgModule({
   declarations: [
     DetailComponent,
-    BackBtnComponent
+    BackBtnComponent,
+    DetailPipe
   ],
   imports: [
     CommonModule,
